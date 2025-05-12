4f80e803d02af1960b468879317c437aca0fcf29
atparodi10 <joatorres@uamv.edu.ni>
2025-05-11 21:13:12 -0600

    Ejercicios Semana 7

--- While/while8.py
+++ While/while8.py
@@ -10,7 +10,7 @@ while True:
         temp = num
     
   
-    if num.replace(".", "").isdigit():
+    if temp.replace(".", "").isdigit():
         num = float(num)
         break
 
@@ -19,7 +19,15 @@ while True:
     
 frecuencia = [0] * 10
 
+num_solo_digito = temp.replace("-", "").replace(".", "")
+
 i = 0
 
-while i < len(num):
+while i < len(num_solo_digito):
+    digito = int(num_solo_digito[i])
+    frecuencia[digito] += 1
+    i += 1
+
+for i in range(10):
+    print(f"Digito {i}: {frecuencia[i]} veces")
     
